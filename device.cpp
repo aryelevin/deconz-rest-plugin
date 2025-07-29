220cec40078fe194b25a715d2810ecdbca4a6c8a
Arye Levin <aryelevin@me.com>
2025-07-30 01:09:18 +0300

    fix debug

--- device.cpp
+++ device.cpp
@@ -786,7 +786,7 @@ void DEV_BasicClusterStateHandler(Device *device, const Event &event)
             }
         }
     }
-    else if (event.what() == REventZclResponse && !event.hasData() && event.resource)
+    else if (event.what() == REventZclResponse /*&& !event.hasData()*/)
     {
         DBG_Printf(DBG_DEV, "DEV received event.what() %s: " FMT_MAC ", event.hasData %i\n", event.what(), FMT_MAC_CAST(device->key()), event.hasData());
         d->setState(DEV_InitStateHandler); // ok re-evaluate
